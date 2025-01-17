969faeb7b937b2a8d19ac043cc60f74a4af954c1
ankit <2017araj@gmail.com>
2025-01-17 16:02:09 +0530

    leetcode55

--- src/main/java/org/example/leetcode/leetcode55.java
+++ src/main/java/org/example/leetcode/leetcode55.java
@@ -4,14 +4,14 @@ public class leetcode55 {
     public boolean canJump(int[] nums) {
         int n = nums.length;
         int maxJump = 0;
-        for(int i=0 ; i<n; i++){
+        for (int i = 0; i < n; i++) {
 
             if (i > maxJump) {
                 return false;
             }
 
-            maxJump = Math.max(maxJump, nums[i]+i);
-            if(maxJump>=n-1){
+            maxJump = Math.max(maxJump, nums[i] + i);
+            if (maxJump >= n - 1) {
                 return true;
             }
         }
